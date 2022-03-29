7e0b332b387faa01638db4eb272340779c121609
Damion U <s1107777@student.hsleiden.nl>
2022-03-29 14:13:20 +0200

    secondes bijhouden

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const puzzel1Button = document.getElementById('js--puzzel1Button');
 const puzzel2Button = document.getElementById('js--puzzel2Button');
 const puzzel3Button = document.getElementById('js--puzzel3Button');
 
+var startTime, endTime;
+
+
 if(document.URL.includes('dierentuinpad.html')){
     puzzel2Button.style.cursor = 'default';
     puzzel3Button.style.cursor = 'default';
@@ -18,5 +21,19 @@ if(document.URL.includes('dierentuinpad-4.html')){
     puzzel1Button.style.cursor = 'default';
     puzzel2Button.style.cursor = 'default';
     puzzel3Button.style.cursor = 'default';
+}
+
+function start(){
+    startTime = new Date();
+}
+
+function end(){
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //ms
+    timeDiff /= 1000;
+    let seconds = Math.round(timeDiff);
+
+    window.location="dierentuinpad-2.html";
+    console.log('seconde: ' + seconds);
 }
 
