8396b66804a81bab83a2d5251d0b3df7f3d6beaa
Daan <daan.passchier@xs4all.nl>
2022-03-29 14:13:12 +0200

    front gate added + fade in/out

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAH");
+const overlay = document.getElementById("js--overlay");
+const audio = document.getElementById("js--audio");
+const gate = document.getElementById("js--gate");
+const arrow = document.getElementById("js--arrow");
+
+const doOverlay = () => {
+    overlay.style.zIndex = "1";
+    overlay.style.opacity = "1";
+    audio.play();
+
+    audio.onended = () => {
+        overlay.style.opacity = "0";
+        gate.src = "/images/zoo_gate_back.jpg";
+        arrow.style.display = "none";
+    }
+}
