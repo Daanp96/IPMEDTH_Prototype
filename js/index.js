bdad52ca6845d26504955c16faf81bc9b3f14abe
Damion U <s1107777@student.hsleiden.nl>
2022-03-29 11:12:59 +0200

    first elements added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAH");
+const puzzel1Button = document.getElementById('js--puzzel1Button');
+const puzzel2Button = document.getElementById('js--puzzel2Button');
+const puzzel3Button = document.getElementById('js--puzzel3Button');
+
+if(document.URL.includes('dierentuinpad')){
+    puzzel2Button.style.cursor = 'default';
+    puzzel3Button.style.cursor = 'default';
+}
+
