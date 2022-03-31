77da5a35139fe837100c52e613c7d56c93b57e80
TjalleS <44362860+TjalleS@users.noreply.github.com>
2022-03-31 09:53:38 +0200

    button next show up after puzzel done

--- js/drag&drop.js
+++ js/drag&drop.js
@@ -2,10 +2,20 @@ const introPage = new Audio('../audio/puzzelAapIntro.mp3');
 const alertGood = new Audio('../audio/puzzelAlertGood.mp3'); 
 const alertWrong = new Audio('../audio/puzzelAlertWrong.mp3');
 
-const backdropMonkeyLB = document.getElementById("div1").style.opacity = "0.5"; 
-const backdropMOnkeyLO = document.getElementById("div2").style.opacity = "0.5";
-const backdropMOnkeyRB = document.getElementById("div3").style.opacity = "0.5"; 
-const backdropMOnkeyRO = document.getElementById("div4").style.opacity = "0.5"; 
+const backdropMonkeyLB = document.getElementById("div1");
+backdropMonkeyLB.style.opacity = "0.5";
+
+const backdropMOnkeyLO = document.getElementById("div2");
+backdropMOnkeyLO.style.opacity = "0.5";
+
+const backdropMOnkeyRB = document.getElementById("div3");
+backdropMOnkeyRB.style.opacity = "0.5"; 
+
+const backdropMOnkeyRO = document.getElementById("div4");
+backdropMOnkeyRO.style.opacity = "0.5"; 
+
+const btnNext = document.getElementById("btnPuzzelAap");
+btnNext.style.opacity = "0";
 
 function intro(){
     introPage.play();
@@ -18,43 +28,55 @@ window.onload = function(){
 function good(){
     alertGood.play();
 }
-function wrong(){
+function wrong()
+{
     alertWrong.play();
 }
 
 function allowDrop(e)
 {
-  e.preventDefault();
+    e.preventDefault();
 }
 
 function drag(e)
 {
-  e.dataTransfer.setData("content",e.target.id);
+    e.dataTransfer.setData("content",e.target.id);
 }
 
 function drop(e)
 {
-  e.preventDefault();
-    var image =e.dataTransfer.getData("content");
-  if (e.target.id === document.getElementById(image).getAttribute('data-div')){ 
-    // alert("ok");
-    good();
-    e.target.appendChild(document.getElementById(image));
-    let changeOpacity = document.getElementById(e.target.id);
-    
-    changeOpacity.style.opacity = "1";
-    
-    // react shit
-    // this.setState({count : this.state.count + 1})
-    // console.log(this.state.count);
+    e.preventDefault();
+        var image =e.dataTransfer.getData("content");
+    if (e.target.id === document.getElementById(image).getAttribute('data-div')){ 
 
-    // if(this.state.count === 3){
-    // let showButton = document.getElementById("btnPuzzelAap");
-    // showButton.style.display = "block";
-    // }
-  }
-  else{
-    // alert("wrong");
-    wrong();
-  }
+        good();
+        // alert("goed zo!");
+        e.target.appendChild(document.getElementById(image));
+        let changeOpacity = document.getElementById(e.target.id);
+        
+        changeOpacity.style.opacity = "1";
+        
+        // react shit
+        // this.setState({count : this.state.count + 1})
+        // console.log(this.state.count);
+
+        // if(this.state.count === 3){
+        // let showButton = document.getElementById("btnPuzzelAap");
+        // showButton.style.display = "block";
+        // }
+    }
+    else{
+        wrong();
+        // alert("nee, die is fout");
+     }
+    if(backdropMonkeyLB.style.opacity === "1" && backdropMOnkeyLO.style.opacity === "1" && backdropMOnkeyRB.style.opacity === "1" && backdropMOnkeyRO.style.opacity === "1"){
+        console.log("werkt")
+        btnNext.style.opacity = "1";
+    
+}
+
+}
+
+function showButton(){
+
 }
