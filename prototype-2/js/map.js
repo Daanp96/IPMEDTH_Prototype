303d65a986b1d3b42587f1964d96fb4b671eb726
Damion U <s1107777@student.hsleiden.nl>
2022-03-31 15:24:27 +0200

    prototype 2 is nu 1 geheel

--- prototype-2/js/map.js
+++ prototype-2/js/map.js
@@ -1,6 +1,7 @@
 const puzzel1Button = document.getElementById('js--puzzel1Button');
 const puzzel2Button = document.getElementById('js--puzzel2Button');
 const puzzel3Button = document.getElementById('js--puzzel3Button');
+const newChapterButton = document.getElementById("js--newChapterButton");
 
 const formSubmit = document.getElementById("js--form-submit");
 const formSubmitDragnDrop = document.getElementById("btnPuzzelAap");
@@ -8,56 +9,32 @@ const formSubmitDragnDrop = document.getElementById("btnPuzzelAap");
 let aantalSecondesSavanneRekensom;
 let aantalSecondesSavanneFoutdier;
 let aantalSecondesSavanneDragnDrop;
+
 var startTime, endTime;
 
-// if(document.URL.includes('dierentuinpad.html')){
-//     puzzel2Button.style.cursor = 'default';
-//     puzzel3Button.style.cursor = 'default';
-// }
 if(document.URL.includes('dierentuinpad.html')){
-    // puzzel1Button.style.cursor = 'default';
-    // puzzel3Button.style.cursor = 'default';
-    console.log(localStorage.getItem('aantalSecondesSavanneRekensom'));
-    console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
-    console.log(localStorage.getItem('aantalSecondesSavanneDragnDrop'));
-
-    // if(aantalSecondesSavanneRekensom !== null){
-    //     console.log(localStorage.getItem('aantalSecondesSavanneRekensom'));
-    // }
-    // if(aantalSecondesSavanneFoutdier == null){
-    //     console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
-    // }
-    // if(aantalSecondesSavanneDragnDrop == null){
-    //     console.log(localStorage.getItem('aantalSecondesSavanneDragnDrop'));
-    // }
+    if(localStorage.getItem('aantalSecondesSavanneRekensom') != null){
+        console.log(localStorage.getItem('aantalSecondesSavanneRekensom'));
+    }
+    if(localStorage.getItem('aantalSecondesSavanneFoutdier') != null){
+        console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
+    }
+    if(localStorage.getItem('aantalSecondesSavanneDragnDrop') != null){
+        console.log(localStorage.getItem('aantalSecondesSavanneDragnDrop'));
+    }
+    if(localStorage.getItem('aantalSecondesSavanneRekensom') != null && localStorage.getItem('aantalSecondesSavanneFoutdier') != null && localStorage.getItem('aantalSecondesSavanneDragnDrop') != null){
+        newChapterButton.style.opacity = 1;
+    }
 }
-
-// if(document.URL.includes('dierentuinpad-3.html')){
-//     // puzzel1Button.style.cursor = 'default';
-//     // puzzel2Button.style.cursor = 'default';
-
-//     console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
-// }
-// if(document.URL.includes('dierentuinpad-4.html')){
-    // console.log(localStorage.getItem('aantalSecondesSavanneDragnDrop'));
-
-//     puzzel1Button.style.cursor = 'default';
-//     puzzel2Button.style.cursor = 'default';
-//     puzzel3Button.style.cursor = 'default';
-// }
 
 function start(){
     startTime = new Date();
 }
-// toevoegen wanneer prototype begint
-// na alle 3 de opdrachten -> rechtsbovenin button
-// om de tijd te stoppen --> dit doen wij
-function startGeheleTijd(){
-    startTime = new Date();
-}
 
 if(document.URL.includes('puzzel-savanne-rekensom.html')){
+    
     formSubmit.addEventListener("click", function(e){
+        
         endTime = new Date();
         var timeDiff = endTime - startTime; //ms
         timeDiff /= 1000;
@@ -75,6 +52,7 @@ if(document.URL.includes('puzzel-savanne-rekensom.html')){
             try {
                 localStorage.setItem("aantalSecondesSavanneRekensom", aantalSecondesSavanneRekensom);
                 console.log(localStorage.getItem('aantalSecondesSavanneRekensom'));
+
             } catch (error) {
                 console.log(error);
             }
@@ -95,6 +73,7 @@ if(document.URL.includes('puzzel-savanne-rekensom.html')){
 
 if(document.URL.includes('puzzel-savanne-foutDier.html')){
     formSubmit.addEventListener("click", function(e){
+        
         endTime = new Date();
         var timeDiff = endTime - startTime; //ms
         timeDiff /= 1000;
@@ -106,12 +85,14 @@ if(document.URL.includes('puzzel-savanne-foutDier.html')){
         if(seconds <= 2){
             localStorage.setItem("aantalSecondesSavanneFoutdier", aantalSecondesSavanneFoutdier);
             console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
+
         }
         // if(seconds > 60 && seconds <= 120){
         if(seconds > 2 && seconds <= 7){
             try {
                 localStorage.setItem("aantalSecondesSavanneFoutdier", aantalSecondesSavanneFoutdier);
                 console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
+
             } catch (error) {
                 console.log(error);
             }
@@ -121,6 +102,7 @@ if(document.URL.includes('puzzel-savanne-foutDier.html')){
             try {
                 localStorage.setItem("aantalSecondesSavanneFoutdier", aantalSecondesSavanneFoutdier);
                 console.log(localStorage.getItem('aantalSecondesSavanneFoutdier'));
+
                 
             } catch (error) {
                 console.log(error);
@@ -177,4 +159,11 @@ function endSavanneRekensom(){
 function endSavanneFoutdier(){
     window.location.href="dierentuinpad.html";  
 }
+function endSavanneDragnDrop(){
+    window.location.href="dierentuinpad.html"; 
+}
+
+// newChapterButton.addEventListener("click", function(e){
+//     window.location.href="../../index2.html"; 
+// });
 
